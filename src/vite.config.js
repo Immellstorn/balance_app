502d9ccd8a811e71a396824b100df8928b4a9e52
Immellstorn <Immellstorn7@mail.ru>
2025-10-28 15:36:43 +0300

    change source

--- src/vite.config.js
+++ src/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/src.css', 'resources/js/src.js'],
             refresh: true,
         }),
         tailwindcss(),
